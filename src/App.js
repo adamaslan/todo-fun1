a045f3b98f69872691a81e4df01df996bb7a1b75
Adam <chillcoders@gmail.com>
2022-03-25 12:22:27 -0400

    timer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Counter from "./components/Counter";
+import Timer from "./components/Timer";
 import React from "react";
 function App() {
   return (
     <div>
       <Counter />
       <div className="fun">Sup Dog, What do you have to do?</div>
+      <Timer />
     </div>
   );
 }
